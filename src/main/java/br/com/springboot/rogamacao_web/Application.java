76350e8ab4305d0e65dc85460ff6522e288194d6
stenio medeiros <steniomedeiro85@gmail.com>
2024-07-09 22:26:23 -0300

    Commit inicial

---
+++ src/main/java/br/com/springboot/rogamacao_web/Application.java
@@ -0,0 +1,15 @@
+package br.com.springboot.rogamacao_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class@SpringBootApplication
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
